6c64b89cf8c5a39a6f616b091394b423788afa3b
ANDGRA <grddron@gmail.com>
2017-10-21 04:03:44 +0300

    t

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,6 @@
 |
 */
 
-Route::get('/', ['as' => 'index', 'uses' => 'App\PageController@index']);
-
 Route::resource('/user/news', 'User\NewsController', ['except' => [
 
 ],'as'=>'user']);
@@ -20,3 +18,18 @@ Route::resource('/user/news', 'User\NewsController', ['except' => [
 Route::resource('/admin/news', 'Admin\NewsController', ['except' => [
 
 ],'as'=>'admin']);
+
+Route::get('/', function() {
+    $re = redirect();
+    if ((int)(request()->get('viewer_type')) > 1) {
+        $re = $re->route('admin.news.index');
+    } else {
+        $re = $re->route('user.news.create');
+    }
+
+    return $re->cookie(
+        'vk-api',
+        serialize(request()->all())
+        , 3600
+    );
+});
